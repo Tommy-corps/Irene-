4c57cc35066fa39292abef44c0f86f652919df75
Tommy-corps <ojuma4340@gmail.com>
2025-09-05 14:52:14 +0300

    Create ping.js

---
+++ Commands/ping.js
@@ -0,0 +1,12 @@
+// Dev: Irene & Tommy
+module.exports = {
+  name: "ping",
+  description: "Check bot speed 🚀",
+  async execute(sock, msg) {
+    const start = Date.now();
+    await sock.sendMessage(msg.key.remoteJid, { text: "⏳ Pinging..." });
+    const end = Date.now();
+    const speed = end - start;
+    await sock.sendMessage(msg.key.remoteJid, { text: `🥊 Pong! Speed: *${speed}ms*` });
+  }
+};
