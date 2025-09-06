ab198e6f98e0a8846b97dc2aeda5818e8efd0967
Tommy-corps <ojuma4340@gmail.com>
2025-09-06 08:02:35 +0300

    Update owner.js

--- Commands/owner.js
+++ Commands/owner.js
@@ -4,8 +4,11 @@ module.exports = {
   description: "Show owner number 👑",
   async execute(sock, msg) {
     const STATUS_REACT = ["👍","🔥","❤️","😂","😍","😎","✨","👏","💯"];
-    await sock.sendMessage(msg.key.remoteJid, { react: { text: STATUS_REACT[Math.floor(Math.random()*STATUS_REACT.length)], key: msg.key } });
+    await sock.sendMessage(msg.key.remoteJid, { 
+      react: { text: STATUS_REACT[Math.floor(Math.random() * STATUS_REACT.length)], key: msg.key } 
+    });
 
-    await sock.sendMessage(msg.key.remoteJid, { text: "👑 Owner: wa.me/255624236654" });
+    const text = `👑 *BOT OWNER*\n\n📞 wa.me/255624236654\n🤖 Powered by *Irene Bot*`;
+    await sock.sendMessage(msg.key.remoteJid, { text });
   }
 };
