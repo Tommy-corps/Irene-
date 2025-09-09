f2b5b87caae9ea8b3b12accecc3c049ada68b616
Tommy-corps <ojuma4340@gmail.com>
2025-09-09 13:29:43 +0300

    Delete Commands directory

--- Commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "ping",
-  description: "Test responsiveness",
-  async execute(sock, msg, args, settings) {
-    await sock.sendMessage(msg.key.remoteJid, { text: "🏓 Pong!" });
-  }
-};
