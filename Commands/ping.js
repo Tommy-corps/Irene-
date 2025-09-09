a70e30e17213ff91a80e90158a8cdca986937891
Tommy-corps <ojuma4340@gmail.com>
2025-09-09 08:22:12 +0300

    Update ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,14 +1,7 @@
-// Dev: Irene & Tommy
 module.exports = {
   name: "ping",
-  description: "Check bot speed 🚀",
-  async execute(sock, msg) {
-    const start = Date.now();
-    await sock.sendMessage(msg.key.remoteJid, { text: "⏳ Pinging..." });
-    const end = Date.now();
-    const speed = end - start;
-    await sock.sendMessage(msg.key.remoteJid, { text: `🥊 Pong! Speed: *${speed}ms*` });
-    
-   //🚀_____________DEV IRENE❤️_______________🚀
+  description: "Test responsiveness",
+  async execute(sock, msg, args, settings) {
+    await sock.sendMessage(msg.key.remoteJid, { text: "🏓 Pong!" });
   }
 };
