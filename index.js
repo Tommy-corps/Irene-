e30c0c3166c9ee44079b251ccc6754996122eba6
Tommy-corps <ojuma4340@gmail.com>
2025-09-11 18:45:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
+const os = require("os");
 const P = require("pino");
 const express = require("express");
 const {
